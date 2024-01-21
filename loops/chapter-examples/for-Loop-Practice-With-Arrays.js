cdaef0ba9b3999e1673ff7ca4f8320aff27ab2f3
Willjorn <klpumagoi@gmail.com>
2024-01-20 19:27:52 -0600

    finished

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,6 @@
 // create an array variable containing the names
-
+let languages = ["JS", "Java","C#","Python"];
 // write a for loop that prints each name on a different line
+for (let i = 0; i < languages.length; i++){
+    console.log(languages[i]);
+}
--- loops/chapter-examples/for-Loop-Practice-With-Strings.js
+++ loops/chapter-examples/for-Loop-Practice-With-Strings.js
@@ -1,4 +1,10 @@
 // Create a string variable containing your name.
+let name = "Kareem";
+
 
 
 // Write a for loop that prints each character in your name on a different line.
+
+for (let i = 0; i < name.length; i++) {
+    console.log(name[i]);
+}
