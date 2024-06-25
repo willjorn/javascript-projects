5ac5afa8a94abcb1bc478915d7787dbe77542c39
Willjorn <klpumagoi@gmail.com>
2024-06-24 19:24:49 -0500

    finished

--- loops/studio/solution.js
+++ loops/studio/solution.js
@@ -2,12 +2,11 @@ const input = require('readline-sync');
 
 // Part A: #1 Populate these arrays
 
-let protein =['chicken', 'pork', 'tofu', 'beef', 'fish', 'beans'];
+let protein = ['chicken', 'pork', 'tofu', 'beef', 'fish', 'beans'];
 let grains = ['rice', 'pasta', 'corn', 'potato', 'quinoa', 'crackers'];
 let veggies = ['peas', 'green beans', 'kale', 'edamame', 'broccoli', 'asparagus'];
-let beverages =['juice', 'milk', 'water', 'soy milk', 'soda', 'tea'] ;
+let beverages = ['juice', 'milk', 'water', 'soy milk', 'soda', 'tea'];
 let desserts = ['apple', 'banana', 'more kale', 'ice cream', 'chocolate', 'kiwi'];
-
 
 function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
   let pantry = [protein, grains, veggies, beverages, desserts];
@@ -15,66 +14,46 @@ function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
   
   for (let i = 0; i < numMeals; i++) {
     let meal = [];
-    for (let j = 0 < pantry.length; j++){
-        meal.push(pantry[j][i])
-      
+    for (let j = 0; j < pantry.length; j++) {
+      let randomIndex = Math.floor(Math.random() * pantry[j].length);
+      meal.push(pantry[j][randomIndex]);
     }
     meals.push(meal);
   }
-  /// Part A #2: Write a ``for`` loop inside this function
-  /// Code your solution for part A #2 below this comment (and above the return statement) ... ///
-
 
   return meals;
 }
 
-
 function askForNumber() {
-  numMeals = input.question("How many meals would you like to make?");
+  let numMeals = Number(input.question("How many meals would you like to make? "));
   
-  /// CODE YOUR SOLUTION TO PART B here ///
-while (numMeals < 7 && numMeals < 0 isNaN(numMeals)){
-  numMeals = Number(input.question("Please select a number 1 to 6: "));
-}
-
+  while (isNaN(numMeals) || numMeals < 1 || numMeals > 6) {
+    numMeals = Number(input.question("Please select a number from 1 to 6: "));
+  }
 
   return numMeals;
 }
 
-
 function generatePassword(string1, string2) {
-  let code = '';
-
-  /// Code your Bonus Mission Solution here ///
-
-  return code;
+  let combined = string1 + string2;
+  let shuffled = combined.split('').sort(() => 0.5 - Math.random()).join('');
+  return shuffled;
 }
 
 function runProgram() {
-  
   /// TEST PART A #2 HERE ///
-  /// UNCOMMENT the two lines of code below that invoke the mealAssembly function (starting with 'let meals =') and print the result ///
-  /// Change the final input variable (aka numMeals) here to ensure your solution makes the right number of meals ///
-  /// We've started with the number 2 for now. Does your solution still work if you change this value? ///
-  
-   let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
-   console.log(meals)
-  
+  let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
+  console.log(meals);
 
   /// TEST PART B HERE ///
-  /// UNCOMMENT the next two lines to test your ``askForNumber`` solution ///
-  /// Tip - don't test this part until you're happy with your solution to part A #2 ///
-  
-  // let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
-  // console.log(mealsForX);
+  let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
+  console.log(mealsForX);
 
-    /// TEST PART C HERE ///
-  /// UNCOMMENT the remaining commented lines and change the password1 and password2 strings to ensure your code is doing its job ///
-
-  // let password1 = '';
-  // let password2 = '';
-  // console.log("Time to run the password generator so we can update the menu tomorrow.")
-  // console.log(`The new password is: ${generatePassword(password1, password2)}`);
+  /// TEST PART C HERE ///
+  let password1 = 'flavor';
+  let password2 = 'saver';
+  console.log("Time to run the password generator so we can update the menu tomorrow.");
+  console.log(`The new password is: ${generatePassword(password1, password2)}`);
 }
 
 module.exports = {
@@ -88,3 +67,6 @@ module.exports = {
   generatePassword: generatePassword,
   runProgram: runProgram
 };
+
+runProgram();  // Uncomment this line if you want to run the program directly
+
